96c26b09cad351531090dfa684fab20cdc39a2ee
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-03-08 20:45:15 +0100

    Update all_stars_func.js

--- cmd/all_stars_func.js
+++ cmd/all_stars_func.js
@@ -191,7 +191,7 @@ ovlcmd({
   }
 
   await ovl.sendMessage(ms_org, {
-    video: { url: isGo ? "https://files.catbox.moe/74zbq3.mp4" : "https://files.catbox.moe/7jmwi8.mp4" },
+    video: { url: isGo ? "https://files.catbox.moe/1td1ai.mp4" : "https://files.catbox.moe/7jmwi8.mp4" },
     gifPlayback: true
   });
 
