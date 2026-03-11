3decb9337e057b8af02dae3e44822063307cf471
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-03-11 12:55:20 +0100

    Update myneo_lineup_team.js

--- cmd/myneo_lineup_team.js
+++ cmd/myneo_lineup_team.js
@@ -311,7 +311,7 @@ ovlcmd({
 ⚽Blue Lock: ${data.blue_lock}
 💠Élysium: ${data.elysium}
 ╰───────────────────     
-        *🔷Powered by NEOVERSE™`;
+        *🔷Powered by NEOVERSE™*`;
 
       await ovl.sendMessage(ms_org,{video:{url:"https://files.catbox.moe/yimc4o.mp4"},gifPlayback:true},{quoted:cmd_options.ms});
       return await ovl.sendMessage(ms_org,{image:{url:"https://files.catbox.moe/nyy6fb.jpg"},caption:myn},{quoted:cmd_options.ms});
@@ -486,7 +486,7 @@ ovlcmd({
 🌍+player⚽ → voir le Hero
 
 ╰──────────────────▱▱▱
-                     🔷BLUELOCK⚽
+                     🔷BLUELOCK⚽🥅
                  *powered by NEOVERSE™*`;
     
       return ovl.sendMessage(
