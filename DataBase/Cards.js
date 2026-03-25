177759eea7dc834ddbd59b8cb06a3d24cdbb3108
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-03-25 11:13:14 +0100

    Create Cards.js

---
+++ DataBase/Cards.js
@@ -0,0 +1,202 @@
+const cards =
+{
+  name: "3rdRaikage",
+  grade: "Argent",
+  category: "S",
+  images: "",
+  card: "https://files.catbox.moe/7c3pgw.jpg",
+  Price: "800K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "A",
+  grade: "Argent",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/amfh8w.jpg",
+  Price: "600K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Ace",
+  grade: "Bronze",
+  category: "S+",
+  images: "",
+  card: "https://files.catbox.moe/5vqryh.jpg",
+  Price: "400K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Acnologia",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/dy20sv.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Aiolia",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/2mbwpb.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Akainu",
+  grade: "Argent",
+  category: "S+",
+  images: "",
+  card: "https://files.catbox.moe/qgrflk.jpg",
+  Price: "800K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Akaza",
+  grade: "Bronze",
+  category: "S+",
+  images: "",
+  card: "https://files.catbox.moe/o8waf7.jpg",
+  Price: "450K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Aldebaran",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/nvdw2s.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "AllForOne",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/u18ovi.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "AllMight",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/3x7bn9.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Android16",
+  grade: "Bronze",
+  category: "SS+",
+  images: "",
+  card: "https://files.catbox.moe/tt79m2.jpg",
+  Price: "100NC",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Android17(Dbz)",
+  grade: "Argent",
+  category: "SS",
+  images: "",
+  card: "https://files.catbox.moe/hohfci.jpg",
+  Price: "200NC",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Android17(Dbs)",
+  grade: "Or",
+  category: "SS+",
+  images: "",
+  card: "https://files.catbox.moe/4bl67g.jpg",
+  Price: "420NC",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Android18",
+  grade: "Argent",
+  category: "SS",
+  images: "",
+  card: "https://files.catbox.moe/b7neim.jpg",
+  Price: "190NC",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Android19",
+  grade: "Bronze",
+  category: "SS+",
+  images: "",
+  card: "https://files.catbox.moe/1lao5m.jpg",
+  Price: "90NC",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Aoi",
+  grade: "Bronze",
+  category: "S",
+  images: "",
+  card: "https://files.catbox.moe/0rocs7.jpg",
+  Price: "350K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Aokiji",
+  grade: "Argent",
+  category: "S+",
+  images: "",
+  card: "https://files.catbox.moe/dhy54u.jpg",
+  Price: "800K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Aphrodite",
+  grade: "Or",
+  category: "S-",
+  images: "",
+  card: "https://files.catbox.moe/fairbf.jpg",
+  Price: "1M",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Asta",
+  grade: "Bronze",
+  category: "S",
+  images: "",
+  card: "https://files.catbox.moe/tjsj81.jpg",
+  Price: "300K",
+  Moves: "",
+  Patterns: ""
+},
+{
+  name: "Asta(Arc2)",
+  grade: "Bronze",
+  category: "S+",
+  images: "",
+  card: "https://files.catbox.moe/2cd5ht.jpg",
+  Price: "450K",
+  Moves: "",
+  Patterns: ""
+}
+]
