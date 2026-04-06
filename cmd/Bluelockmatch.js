7e0d9c18b61b6f3bda6c4071b1cc463736879a79
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-04-06 17:20:33 +0100

    Update Bluelockmatch.js

--- cmd/Bluelockmatch.js
+++ cmd/Bluelockmatch.js
@@ -287,6 +287,14 @@ LECTURE MESSAGES
 =================================*/
 async function messageMatch(ms, ovl) {
     if (!ms.message) return;
+// ❌ IGNORER LES MESSAGES SYSTÈME
+    if (
+        ms.message.protocolMessage ||
+        ms.message.senderKeyDistributionMessage ||
+        ms.message.messageContextInfo
+    ) {
+        return;
+    }
 
     const chat = ms.key.remoteJid;
     const match = matchsActifs.get(chat);
@@ -429,7 +437,14 @@ LECTURE DES PAVÉS - TOUR DE CONTRÔLE
 =================================*/
 async function handlePaveGame(ms, ovl) {
     if (!ms.message) return false;
-
+// ❌ IGNORER LES MESSAGES SYSTÈME
+    if (
+        ms.message.protocolMessage ||
+        ms.message.senderKeyDistributionMessage ||
+        ms.message.messageContextInfo
+    ) {
+        return false;
+    }
     const chat = ms.key.remoteJid;
     const match = matchsActifs.get(chat);
     if (!match) return false;
