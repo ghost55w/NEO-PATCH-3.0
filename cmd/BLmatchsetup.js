13f9d96fdeb5027177b1e0588c72322c3c803010
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-04-20 15:04:22 +0100

    Remove unnecessary closing brace in BLmatchsetup.js

--- cmd/BLmatchsetup.js
+++ cmd/BLmatchsetup.js
@@ -637,7 +637,7 @@ Le match commence dans *1 minute* 🥅⚽...`;
     });
 
     match.timerMatch = setTimeout(() => lancerMatch(chat, ovl), 60000);
-}
+
 
 // ===============================
     // 🔥 GESTION PAVÉ NORMAL
