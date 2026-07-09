f13bbadeb0055badfa2ea915ce4a1b51519b5b30
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-07-09 13:19:16 +0100

    Update Bluelockmatch.js

--- cmd/Bluelockmatch.js
+++ cmd/Bluelockmatch.js
@@ -2417,7 +2417,7 @@ else {
     attackerWin = attackTotal >= defenseTotal;
 
 }
-                                     
+        }                                     
 
 // ==========================
 // AUTRES ACTIONS
