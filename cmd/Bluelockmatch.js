56975dbef9d64b88abc1adacd1014a054951427d
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-07-29 05:49:05 +0100

    Update Bluelockmatch.js

--- cmd/Bluelockmatch.js
+++ cmd/Bluelockmatch.js
@@ -6157,26 +6157,6 @@ attacker.stats.lastAction = {
     type: "attaque",
     texte: match.pendingAttack
 };
-
- let resume = "";
-
-const actionText = defense.toLowerCase();
-
-if (actionText.includes("tacle")) {
-    resume = `${defender.nom} tente un tacle pour stopper l'action.`;
-}
-else if (actionText.includes("intercepte")) {
-    resume = `${defender.nom} tente une interception.`;
-}
-else if (actionText.includes("bloque")) {
-    resume = `${defender.nom} tente de fermer l'espace.`;
-}
-else if (actionText.includes("contre")) {
-    resume = `${defender.nom} tente un contre défensif.`;
-}
-else {
-    resume = `${defender.nom} répond à l'action offensive.`;
-}
     
 // 🔄 SOURCE UNIQUE : l'attaquant garde le ballon
 const attackerJid = attacker.id || attacker.jid || match.attacker;
