db4a8bdcd416a04372c82f77b2e0b3b652f7d55a
ghost55w <103336705+ghost55w@users.noreply.github.com>
2026-09-23 21:20:33 +0100

    Update testOpenRouter.js

--- cmd/testOpenRouter.js
+++ cmd/testOpenRouter.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+const { ovlcmd } = require("../lib/ovlcmd");
+
 /**
  * ╔══════════════════════════════════════════════════════╗
  * ║              OPENROUTER TEST COMMAND               ║
